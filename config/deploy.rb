fc99c6c2baf67795d1c406c416c57b716656f6c4
akito1201 <akito.tennis@gmail.com>
2021-01-31 08:57:23 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,23 @@
+# 記述内容
+  #アプリケーション名
+  #gitのレポジトリ  
+  #利用するSCM
+  #タスク
+  #それぞれのタスクで実行するコマンド
+
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.15.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "protospace-31677"
+set :repo_url, "https://github.com/akito1201/protospace-31677.git"
 
 # Default branch is :master
+set :branch, 'master'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/protospace-31677"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -33,7 +42,49 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+  set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# rubyのバージョン
+set :rbenv_ruby, '2.1.3'
+
+#出力するログのレベル。
+set :log_level, :debug
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
