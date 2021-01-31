dfad398263e5ba386f20634e67c172359e4ccdfd
akito1201 <akito.tennis@gmail.com>
2021-01-31 22:32:43 +0900

    comments controller indent

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-   if @comment.save
-    redirect_to  prototype_path(@comment.prototype_id)
-   else
-    @prototype = @comment.prototype
-    @comments = @prototype.comments
-    render "prototypes/show"
-   end
+      if @comment.save
+        redirect_to  prototype_path(@comment.prototype_id)
+      else
+        @prototype = @comment.prototype
+        @comments = @prototype.comments
+        render "prototypes/show"
+      end
   end
 
   private
