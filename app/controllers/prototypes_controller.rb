16cc2f3e1ec41efa8f4072d047abecfae3bf8251
akito1201 <akito.tennis@gmail.com>
2021-01-31 22:33:16 +0900

    prototypes controller indent

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -50,12 +50,10 @@ class PrototypesController < ApplicationController
 
   private
 
-
-
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 
- end
+end
 
